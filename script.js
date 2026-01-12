e89db8a3b79cf8019addafce0f5deb1215d37bcf
Uwiagod <sensigod466@gmail.com>
2026-01-12 20:54:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-/* Particles Config */
 particlesJS("particles-js", {
   "particles": {
-    "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
-    "color": { "value": "#8a2be2" },
+    "number": { "value": 80 },
+    "color": { "value": "#ff0000" },
     "shape": { "type": "circle" },
-    "opacity": { "value": 0.5, "random": false },
-    "size": { "value": 3, "random": true },
-    "line_linked": { "enable": true, "distance": 150, "color": "#8a2be2", "opacity": 0.4, "width": 1 },
-    "move": { "enable": true, "speed": 2, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false }
+    "opacity": { "value": 0.5 },
+    "size": { "value": 3 },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ff0000",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": { "enable": true, "speed": 3 }
   },
   "interactivity": {
-    "detect_on": "canvas",
-    "events": { "onhover": { "enable": true, "mode": "grab" }, "onclick": { "enable": true, "mode": "push" }, "resize": true },
-    "modes": { "grab": { "distance": 140, "line_linked": { "opacity": 1 } } }
-  },
-  "retina_detect": true
-});
-
-/* Smooth Scroll for Buttons */
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+    "events": { "onhover": { "enable": true, "mode": "grab" } }
+  }
 });
